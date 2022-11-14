5730c445fd32e72d99265ede5181cac4975ec000
Oscar-Espinoza <oscardavid2094@gmail.com>
2022-11-14 18:57:12 -0300

    JS eliminated

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-const showHideMenu = () => {
-  const mobileMenu = document.getElementById('menu-mobile');
-  if (mobileMenu.classList.contains('active')) {
-    mobileMenu.classList.remove('active');
-  } else {
-    mobileMenu.classList.add('active');
-  }
-};
+
