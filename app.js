a89e501a81606fe27413e32c95a238f0024e0d57
Oscar-Espinoza <oscardavid2094@gmail.com>
2022-11-14 17:11:58 -0300

    Linter errors fixed 2nd try

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-export const showHideMenu = (currentBtn) => {
+let showHideMenu = ''
+
+showHideMenu = () => {
   const mobileMenu = document.getElementById('menu-mobile');
-  if (currentBtn.id === 'toggle-btn') {
-    mobileMenu.classList.add('active');
-  } else {
+  if (mobileMenu.classList.contains('active')) {
     mobileMenu.classList.remove('active');
+  } else {
+    mobileMenu.classList.add('active');
   }
 };
+
