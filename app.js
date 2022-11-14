76848b8fcb76479e0683531739c42f49ce2b3e37
Oscar-Espinoza <oscardavid2094@gmail.com>
2022-11-14 19:30:20 -0300

    Liter errors are gone for good

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ document.querySelectorAll('.menu-btn').forEach((item) => {
   item.addEventListener('click', () => {
     const menu = document.getElementById('menu-mobile');
     if (menu.classList.contains('active')) {
-      menu.classList.remove('active');  
+      menu.classList.remove('active');
     } else {
       menu.classList.add('active');
-    };
+    }
   });
 });
