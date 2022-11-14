63f4a8d618a5eb728f944d18f4fb67737960ceba
Juank628 <jsanchez_zunino@yahoo.com>
2022-11-14 17:20:01 -0500

    mobile menu javascript

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-
+document.querySelectorAll('.menu-btn').forEach(item => {
+    item.addEventListener("click", (e) => {
+        let menu = document.getElementById("menu-mobile")
+        if(menu.classList.contains("active")) {
+            menu.classList.remove("active")  
+        } else {
+            menu.classList.add("active")
+        }
+    })
+})
