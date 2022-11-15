b0311c813846337e5b5d736cd8d98ceebcaf28fc
Juank628 <jsanchez_zunino@yahoo.com>
2022-11-15 15:41:11 -0500

    Project popup javascript

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+const projectsInfo = [
+  {
+    id: '001',
+    name: 'project 1',
+    description: '1',
+    image: '',
+    technologies: [],
+    linkLive: '',
+    linkSource: ''
+  },
+  {
+    id: '002',
+    name: 'project 2',
+    description: '2',
+    image: '',
+    technologies: [],
+    linkLive: '',
+    linkSource: ''
+  }
+]
+
 document.querySelectorAll('.menu-btn').forEach((item) => {
   item.addEventListener('click', () => {
     const menu = document.getElementById('menu-mobile');
@@ -8,3 +29,17 @@ document.querySelectorAll('.menu-btn').forEach((item) => {
     }
   });
 });
+
+document.querySelectorAll('.see-project').forEach((item) => {
+  item.addEventListener('click', (e) => {
+    const project = projectsInfo.filter(item => item.id === e.currentTarget.id)[0]
+    const title = document.getElementById("pop-up-title")
+    const description = document.getElementById("pop-up-description")
+    const popUp = document.getElementById('pop-up')
+    
+    title.textContent = project.name
+    description.textContent = project.description
+
+    popUp.classList.add('active')
+  })
+})
