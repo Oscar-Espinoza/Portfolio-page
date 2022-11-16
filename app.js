cd4b8c77bdc00edbc8e79f08ebcd0e1a4871fc62
Oscar-Espinoza <oscardavid2094@gmail.com>
2022-11-16 15:48:37 -0300

    Linter errors fixed 1st try

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const projectsInfo = [
     image: 'Media/First-project.png',
     technologies: ['CSS', 'HTML', 'Bootstrap', 'Ruby'],
     linkLive: 'https://oscar-espinoza.github.io/Portfolio-page/',
-    linkSource: 'https://github.com/Oscar-Espinoza/Portfolio-page'
+    linkSource: 'https://github.com/Oscar-Espinoza/Portfolio-page',
   },
   {
     id: 'second-project',
@@ -15,49 +15,49 @@ const projectsInfo = [
     image: '/Media/IMG-first.svg',
     technologies: ['HTML', 'Bootstrap', 'Ruby'],
     linkLive: 'https://oscar-espinoza.github.io/Portfolio-page/',
-    linkSource: 'https://github.com/Oscar-Espinoza/Portfolio-page'
-  },{
+    linkSource: 'https://github.com/Oscar-Espinoza/Portfolio-page',
+  }, {
     id: 'third-project',
     name: 'Data Dashboard Healthcare',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
     image: '/Media/IMG-second.svg',
     technologies: ['HTML', 'Bootstrap', 'Ruby'],
     linkLive: 'https://oscar-espinoza.github.io/Portfolio-page/',
-    linkSource: 'https://github.com/Oscar-Espinoza/Portfolio-page'
-  },{
+    linkSource: 'https://github.com/Oscar-Espinoza/Portfolio-page',
+  }, {
     id: 'fourth-project',
     name: 'Website Protfolio',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
     image: '/Media/IMG-third.svg',
     technologies: ['HTML', 'Bootstrap', 'Ruby'],
     linkLive: 'https://oscar-espinoza.github.io/Portfolio-page/',
-    linkSource: 'https://github.com/Oscar-Espinoza/Portfolio-page'
-  },{
+    linkSource: 'https://github.com/Oscar-Espinoza/Portfolio-page',
+  }, {
     id: 'fifth-project',
     name: 'Profesional Art Printing Data',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
     image: '/Media/IMG-first.svg',
     technologies: ['HTML', 'Bootstrap', 'Ruby'],
     linkLive: 'https://oscar-espinoza.github.io/Portfolio-page/',
-    linkSource: 'https://github.com/Oscar-Espinoza/Portfolio-page'
-  },{
+    linkSource: 'https://github.com/Oscar-Espinoza/Portfolio-page',
+  }, {
     id: 'sixth-project',
     name: 'Data Dashboard Healthcare',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
     image: '/Media/IMG-second.svg',
     technologies: ['HTML', 'Bootstrap', 'Ruby'],
     linkLive: 'https://oscar-espinoza.github.io/Portfolio-page/',
-    linkSource: 'https://github.com/Oscar-Espinoza/Portfolio-page'
-  },{
+    linkSource: 'https://github.com/Oscar-Espinoza/Portfolio-page',
+  }, {
     id: 'seventh-project',
     name: 'Website Protfolio',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
     image: '/Media/IMG-third.svg',
     technologies: ['HTML', 'Bootstrap', 'Ruby'],
     linkLive: 'https://oscar-espinoza.github.io/Portfolio-page/',
-    linkSource: 'https://github.com/Oscar-Espinoza/Portfolio-page'
-  }
-]
+    linkSource: 'https://github.com/Oscar-Espinoza/Portfolio-page',
+  },
+];
 
 document.querySelectorAll('.menu-btn').forEach((item) => {
   item.addEventListener('click', () => {
@@ -72,35 +72,33 @@ document.querySelectorAll('.menu-btn').forEach((item) => {
 
 document.querySelectorAll('.see-project').forEach((item) => {
   item.addEventListener('click', (e) => {
-    const popUp = document.getElementById('pop-up')
-    const project = projectsInfo.filter(item => item.id === e.currentTarget.parentNode.id)[0]
-    const title = popUp.querySelector("#pop-up-title")
-    const description = popUp.querySelector("#description-text")
-    const technologiesList = popUp.querySelector('#pop-up-technologies')
-    const img = popUp.querySelector('#pop-up-img')
-    const seeLive = popUp.querySelector('#see-live')
-    const seeSource = popUp.querySelector('#see-source')
+    const popUp = document.getElementById('pop-up');
+    const project = projectsInfo.filter((item) => item.id === e.currentTarget.parentNode.id)[0];
+    const title = popUp.querySelector('#pop-up-title');
+    const description = popUp.querySelector('#description-text');
+    const technologiesList = popUp.querySelector('#pop-up-technologies');
+    const img = popUp.querySelector('#pop-up-img');
+    const seeLive = popUp.querySelector('#see-live');
+    const seeSource = popUp.querySelector('#see-source');
     
-    title.textContent = project.name
-    description.textContent = project.description
+    title.textContent = project.name;
+    description.textContent = project.description;
     if(technologiesList.hasChildNodes()){
-      
       Array.from(technologiesList.children).forEach((child) => {
-        technologiesList.removeChild(child)
-      })
+        technologiesList.removeChild(child);
+      });
     }
     project.technologies.forEach((technology) => {
-      technologiesList.appendChild(document.createElement('li')).textContent = technology
-    })
+      technologiesList.appendChild(document.createElement('li')).textContent = technology;
+    });
 
-    img.src = project.image
-    seeLive.href = project.linkLive
-    seeSource.href = project.linkSource
-
-    popUp.classList.add('active')
-  })
-})
+    img.src = project.image;
+    seeLive.href = project.linkLive;
+    seeSource.href = project.linkSource;
+    popUp.classList.add('active');
+  });
+});
 
 document.getElementById('close-window').addEventListener('click', () => {
-  document.getElementById('pop-up').classList.remove('active')
-})
+  document.getElementById('pop-up').classList.remove('active');
+});
