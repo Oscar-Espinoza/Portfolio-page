4cf270df196ada85434f5cab742c2b35be02b8ee
Oscar-Espinoza <oscardavid2094@gmail.com>
2022-11-16 15:17:33 -0300

    Here we go linters

--- app.js
+++ app.js
@@ -1,21 +1,61 @@
 const projectsInfo = [
   {
-    id: '001',
-    name: 'project 1',
-    description: '1',
-    image: '',
-    technologies: [],
-    linkLive: '',
-    linkSource: ''
+    id: 'first-project',
+    name: 'Multi-Post Stories',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    image: 'Media/First-project.png',
+    technologies: ['CSS', 'HTML', 'Bootstrap', 'Ruby'],
+    linkLive: 'https://oscar-espinoza.github.io/Portfolio-page/',
+    linkSource: 'https://github.com/Oscar-Espinoza/Portfolio-page'
   },
   {
-    id: '002',
-    name: 'project 2',
-    description: '2',
-    image: '',
-    technologies: [],
-    linkLive: '',
-    linkSource: ''
+    id: 'second-project',
+    name: 'Profesional Art Printing Data',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    image: '/Media/IMG-first.svg',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    linkLive: 'https://oscar-espinoza.github.io/Portfolio-page/',
+    linkSource: 'https://github.com/Oscar-Espinoza/Portfolio-page'
+  },{
+    id: 'third-project',
+    name: 'Data Dashboard Healthcare',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    image: '/Media/IMG-second.svg',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    linkLive: 'https://oscar-espinoza.github.io/Portfolio-page/',
+    linkSource: 'https://github.com/Oscar-Espinoza/Portfolio-page'
+  },{
+    id: 'fourth-project',
+    name: 'Website Protfolio',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    image: '/Media/IMG-third.svg',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    linkLive: 'https://oscar-espinoza.github.io/Portfolio-page/',
+    linkSource: 'https://github.com/Oscar-Espinoza/Portfolio-page'
+  },{
+    id: 'fifth-project',
+    name: 'Profesional Art Printing Data',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    image: '/Media/IMG-first.svg',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    linkLive: 'https://oscar-espinoza.github.io/Portfolio-page/',
+    linkSource: 'https://github.com/Oscar-Espinoza/Portfolio-page'
+  },{
+    id: 'sixth-project',
+    name: 'Data Dashboard Healthcare',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    image: '/Media/IMG-second.svg',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    linkLive: 'https://oscar-espinoza.github.io/Portfolio-page/',
+    linkSource: 'https://github.com/Oscar-Espinoza/Portfolio-page'
+  },{
+    id: 'seventh-project',
+    name: 'Website Protfolio',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    image: '/Media/IMG-third.svg',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    linkLive: 'https://oscar-espinoza.github.io/Portfolio-page/',
+    linkSource: 'https://github.com/Oscar-Espinoza/Portfolio-page'
   }
 ]
 
@@ -32,14 +72,35 @@ document.querySelectorAll('.menu-btn').forEach((item) => {
 
 document.querySelectorAll('.see-project').forEach((item) => {
   item.addEventListener('click', (e) => {
-    const project = projectsInfo.filter(item => item.id === e.currentTarget.id)[0]
-    const title = document.getElementById("pop-up-title")
-    const description = document.getElementById("pop-up-description")
     const popUp = document.getElementById('pop-up')
+    const project = projectsInfo.filter(item => item.id === e.currentTarget.parentNode.id)[0]
+    const title = popUp.querySelector("#pop-up-title")
+    const description = popUp.querySelector("#description-text")
+    const technologiesList = popUp.querySelector('#pop-up-technologies')
+    const img = popUp.querySelector('#pop-up-img')
+    const seeLive = popUp.querySelector('#see-live')
+    const seeSource = popUp.querySelector('#see-source')
     
     title.textContent = project.name
     description.textContent = project.description
+    if(technologiesList.hasChildNodes()){
+      
+      Array.from(technologiesList.children).forEach((child) => {
+        technologiesList.removeChild(child)
+      })
+    }
+    project.technologies.forEach((technology) => {
+      technologiesList.appendChild(document.createElement('li')).textContent = technology
+    })
+
+    img.src = project.image
+    seeLive.href = project.linkLive
+    seeSource.href = project.linkSource
 
     popUp.classList.add('active')
   })
+})
+
+document.getElementById('close-window').addEventListener('click', () => {
+  document.getElementById('pop-up').classList.remove('active')
 })
